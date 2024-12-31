b5529e3bbaa572861214b98df00876258a7352e0
wlr1 <uns4d123@gmail.com>
2024-12-31 22:25:10 +0200

    ADDED: cors settings

--- server/main.go
+++ server/main.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"server/initializers"
+	"time"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
 
-	fmt.Print("gdfgdfg")
+	r := gin.Default()
+
+	r.ForwardedByClientIP = true
+	err := r.SetTrustedProxies([]string{"127.0.0.1"})
+	if err != nil {
+		return
+	}
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8000", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
+
+	log.Fatal(r.Run())
 
 }
