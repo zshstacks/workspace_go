302915cd80adce93779c4523fe1b516bdc43a836
wlr1 <uns4d123@gmail.com>
2025-01-12 01:15:36 +0200

    .gitignore

--- client/src/app/components/Pomodoro/Pomodoro.tsx
+++ client/src/app/components/Pomodoro/Pomodoro.tsx
@@ -10,3 +10,25 @@ const Pomodoro = () => {
 };
 
 export default Pomodoro;
+
+{
+  /* <div className="min-h-screen relative overflow-hidden">
+     
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        autoPlay
+        loop
+      >
+        <source src="/uploads/amsterdam.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+     
+      <div className="relative z-10">
+        <PomodoroContent />
+      </div>
+
+      
+      <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-5"></div>
+    </div>  */
+}
