332044e9674b0f14807864ae77a687a88b4e8e53
wlr1 <uns4d123@gmail.com>
2025-03-06 17:25:03 +0200

    ADDED[stats]: stats model

---
+++ server/models/userStatsModel.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type StatsModel struct {
+	gorm.Model
+	UserID         uint `gorm:"uniqueIndex"`
+	CurrentStreak  int
+	HighestStreak  int
+	LastVisitDate  time.Time
+	TotalVisitDays int
+}
